a6bc28720417e77922771108f7b1af5f8dba5540
Екатерина Святкина <99070196+katerina-svyatkina@users.noreply.github.com>
2022-02-10 17:17:38 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-while ( a!=0 && b!=0){
-	if(a>b)
-		a = a % b;
-	else
-		b = b % a;
-}
-	return a + b;
+while (a !=0 && b !=0) {
+        if (a > b)
+            a = a % b;
+        else
+            b = b % a;
+    }
+    return a + b;
 }
